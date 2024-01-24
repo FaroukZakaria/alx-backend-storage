850ecc705951b5a8b985477872b912148c63912f
FaroukZakaria <gameswithoutlose97@gmail.com>
2024-01-24 16:58:36 +0200

    Task 1 - pushed before adding rest of methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,9 +26,14 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
+        return key
+
     def get(
             self, key: str, fn: Optional[Callable] = None) -> Union[
                 str, bytes, int, float]:
+        """
+        Get method (callable optional function)
+        """
         data = self._redis.get(key)
 
         if data is None:
@@ -37,4 +42,16 @@ class Cache():
         if fn is not None:
             return fn(data)
 
-        return key
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        get string method
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        get integer method
+        """
+        return self.get(key, fn=int)
